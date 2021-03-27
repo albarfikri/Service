76ae57a5896ddba2b58675bf821726599050aa2d
albarfikri4 <fikrialbar4@gmail.com>
2021-03-27 22:41:23 +0700

    Bound Service finished

--- app/src/main/java/com/example/myservice/MainActivity.kt
+++ app/src/main/java/com/example/myservice/MainActivity.kt
@@ -1,36 +1,67 @@
 package com.example.myservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import com.example.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    //bound service
+    private var mServiceBound = false
+    private lateinit var mBoundService: MyBoundService
+
+    // merupakan listener untuk menerima callback dari service connection
+    private val mServiceConnection = object : ServiceConnection {
+
+        //callback setelah terputus
+        override fun onServiceDisconnected(name: ComponentName) {
+            mServiceBound = false
+        }
+
+        //callback terhubung
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val myBinder = service as MyBoundService.MyBinder
+            mBoundService = myBinder.getService
+            mServiceBound = true
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnStartService.setOnClickListener{
-            val mStartServiceIntent = Intent(this,MyService::class.java)
+        binding.btnStartService.setOnClickListener {
+            val mStartServiceIntent = Intent(this, MyService::class.java)
             startService(mStartServiceIntent)
         }
 
-        binding.btnStartJobIntentService.setOnClickListener{
+        binding.btnStartJobIntentService.setOnClickListener {
             val mStartIntentService = Intent(this, MyJobIntentService::class.java)
             mStartIntentService.putExtra(MyJobIntentService.EXTRA_DURATION, 5000L)
             MyJobIntentService.enqueueWork(this, mStartIntentService)
         }
 
-        binding.btnStartBoundService.setOnClickListener{
-
+        binding.btnStartBoundService.setOnClickListener {
+            val mBoundServiceIntent = Intent(this, MyBoundService::class.java)
+            bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE)
         }
 
-        binding.btnStopBoundService.setOnClickListener{
+        binding.btnStopBoundService.setOnClickListener {
+            unbindService(mServiceConnection)
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mServiceBound) {
+            unbindService(mServiceConnection)
         }
     }
 }
